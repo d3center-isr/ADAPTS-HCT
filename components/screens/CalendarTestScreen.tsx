8b41c9d8a1e8aea9ebfed4ac97b00fceac9970c4
Ofek-Shani <ofek.a.shani@gmail.com>
2025-07-21 17:32:32 -0400

    create TextButton, replace most pressables with text inside with TextButton component, move multiple components into new 'common' folder

--- components/screens/CalendarTestScreen.tsx
+++ components/screens/CalendarTestScreen.tsx
@@ -1,7 +1,7 @@
 import { View } from "react-native";
 import { useState } from "react";
 import { Text } from "react-native";
-import CalendarContainer from "../CalendarContainer";
+import CalendarContainer from "../common/CalendarContainer";
 
 /**
  * This is a test object that simply changes color to 1 of 3 values depending on which 
