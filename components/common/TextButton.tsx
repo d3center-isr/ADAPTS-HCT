f35e1671295708515bd0f2d6a06a0ff423804134
Ofek-Shani <ofek.a.shani@gmail.com>
2025-07-23 11:49:04 -0400

    Add documentation to text button

--- components/common/TextButton.tsx
+++ components/common/TextButton.tsx
@@ -1,3 +1,7 @@
+/**
+ * A common interface to create a button containing text.
+ * If you need any kind of button, use this component. 
+*/
 import React from 'react';
 import { GestureResponderEvent } from 'react-native';
 import { Pressable, Text, StyleProp, ViewStyle, TextStyle, StyleSheet } from 'react-native';
@@ -8,7 +12,14 @@ type TextButtonProps = {
     buttonStyle?: StyleProp<ViewStyle>;
     textStyle?: StyleProp<TextStyle>;
 };
-
+/**
+ * A common interface to create a button containing text.
+ * If you need any kind of button, use this component. 
+ * @param test {string} The text to display on the button.
+ * @param onPress {(GestureResponderEvent) => void} The function to call once the button is pressed.
+ * @param buttonStyle {StyleProp<ViewStyle>} A set of style overrides for the button
+ * @param textStyle {StyleProp<TextStyle>} A set of style overrides for the text
+ */
 const TextButton: React.FC<TextButtonProps> = ({
     text,
     onPress,
